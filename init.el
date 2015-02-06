05774d582bacb2237f7edb10144def895120974b
mnespor <MattNespor@nanonation.net>
2015-02-06 16:14:04 -0600

    initial commit

---
+++ init.el
@@ -0,0 +1,12 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+(setq live-disable-zone t)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+
